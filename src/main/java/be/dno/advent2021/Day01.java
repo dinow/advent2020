31e6722ab3e4d2f1a4fc5d324c0458d2b195092a
Didier Nowak <didiernowak@gmail.com>
2021-12-01 20:57:55 +0100

    day01 - improve performance

--- src/main/java/be/dno/advent2021/Day01.java
+++ src/main/java/be/dno/advent2021/Day01.java
@@ -4,21 +4,29 @@ package be.dno.advent2021;
 import be.dno.Day;
 
 public class Day01 extends Day {
+	private Integer[] depths;
 
 	public Day01(){
 		fileName = "2021/day01.txt";
 	 }
 
+	 @Override
+	public void fillDataStruct() {
+		depths = new Integer[lines.size()];
+		int i = 0;
+		for(String line : lines) {
+			depths[i] = Integer.valueOf(line);
+			i++;
+		}
+	}
+
 	@Override
 	public String processPart1() {
 		int numberOfIncrease = 0;
-		Integer previousNumber = null;
-		for(String line : lines){
-			Integer lineValue = Integer.valueOf(line);
-			if (previousNumber != null && lineValue > previousNumber){
+		for (int i = 1; i < depths.length; i++){
+			if (depths[i] > depths[i-1]){
 				numberOfIncrease++;
 			}
-			previousNumber = lineValue;
 		}
 		return numberOfIncrease+"";
 	}
@@ -26,24 +34,15 @@ public class Day01 extends Day {
 	@Override  
    	public String processPart2(){
 		int numberOfIncrease = 0;
-		Integer[] slidingWindow = new Integer[3];
-		int indexSlidingWindow = 0;
-		Integer previousSlidingSum = null;
+		Integer nextSlidingSum = null;
 		Integer currentSlidingSum = null;
-		for(String line : lines){
-			Integer lineValue = Integer.valueOf(line);
-			slidingWindow[indexSlidingWindow] = lineValue;
-			indexSlidingWindow++;
-			if (indexSlidingWindow == 3){
-				currentSlidingSum = slidingWindow[0]+slidingWindow[1]+slidingWindow[2];
-				if (previousSlidingSum != null && currentSlidingSum > previousSlidingSum){
-					numberOfIncrease++;
-				}
-				previousSlidingSum = currentSlidingSum;
-				indexSlidingWindow=2;
-				slidingWindow[0] = slidingWindow[1];
-				slidingWindow[1] = slidingWindow[2];
+		currentSlidingSum = depths[0]+depths[1]+depths[2];
+		for (int i = 2; i < depths.length-1; i++){
+			nextSlidingSum = depths[i-1]+depths[i]+depths[i+1];
+			if (nextSlidingSum > currentSlidingSum){
+				numberOfIncrease++;
 			}
+			currentSlidingSum = nextSlidingSum;
 		}
 		return numberOfIncrease+"";
 	}
