5ef867f23a40299e4f5c5c6b3cdc2e2c8ca54c7e
Didier Nowak <didier.nowak@ctie.etat.lu>
2022-12-02 09:09:26 +0100

    cleanup and prepare day03

--- src/main/java/be/dno/advent2022/Day02.java
+++ src/main/java/be/dno/advent2022/Day02.java
@@ -15,7 +15,7 @@ public class Day02 extends Day {
 
    public Day02(){
       fileName = "2022/day02.txt";
-    }
+   }
 
    @Override
    public void fillDataStruct() {
@@ -30,30 +30,23 @@ public class Day02 extends Day {
    public String processPart1() {
       long totalScore = 0l;
       for (String[] game : games){
-         int scoreShape = ((int)game[1].charAt(0))-87; //1, 2, 3
+         int scoreShape    = ((int)game[1].charAt(0))-87;
          int scoreOpponent = ((int)game[0].charAt(0))-64;
          int score = scoreShape;
          if (scoreShape == scoreOpponent){
             score += 3;
          }
-         if (scoreShape == ROCK && scoreOpponent == SCISSOR){
+         if (   (scoreShape == ROCK    && scoreOpponent == SCISSOR)
+             || (scoreShape == PAPER   && scoreOpponent == ROCK)
+             || (scoreShape == SCISSOR && scoreOpponent == PAPER)){
             score += 6;
          }
-         if (scoreShape == PAPER && scoreOpponent == ROCK){
-            score += 6;
-         }
-         if (scoreShape == SCISSOR && scoreOpponent == PAPER){
-            score += 6;
-         }
-         if (scoreShape == ROCK && scoreOpponent == PAPER){
+         if (   (scoreShape == ROCK    && scoreOpponent == PAPER)
+             || (scoreShape == PAPER   && scoreOpponent == SCISSOR)
+             || (scoreShape == SCISSOR && scoreOpponent == ROCK)){
             score += 0;
          }
-         if (scoreShape == PAPER && scoreOpponent == 3){
-            score += 0;
-         }
-         if (scoreShape == SCISSOR && scoreOpponent == ROCK){
-            score += 0;
-         }
+
          totalScore += (long)score;
       }
       return totalScore+"";
@@ -63,58 +56,38 @@ public class Day02 extends Day {
    public String processPart2() {
       long totalScore = 0l;
       for (String[] game : games){
-         int scoreShape = ((int)game[1].charAt(0))-87; //1, 2, 3
+         int scoreShape    = ((int)game[1].charAt(0))-87;
          int scoreOpponent = ((int)game[0].charAt(0))-64;
          switch (scoreShape){
             case 1:
-            //need to lose
-            if (scoreOpponent == ROCK){
-               scoreShape = SCISSOR;
-            }
-            if (scoreOpponent == PAPER){
-               scoreShape = ROCK;
-            }
-            if (scoreOpponent == SCISSOR){
-               scoreShape = PAPER;
-            }
-            break;
+               //need to lose
+               if (scoreOpponent == ROCK)    scoreShape = SCISSOR;
+               if (scoreOpponent == PAPER)   scoreShape = ROCK;
+               if (scoreOpponent == SCISSOR) scoreShape = PAPER;
+               break;
             case 2:
-            //need to draw
-            scoreShape = scoreOpponent;
-            break;
+               //need to draw
+               scoreShape = scoreOpponent;
+               break;
             case 3:
-            //need to win
-            if (scoreOpponent == ROCK){
-               scoreShape = PAPER;
-            }
-            if (scoreOpponent == PAPER){
-               scoreShape = SCISSOR;
-            }
-            if (scoreOpponent == SCISSOR){
-               scoreShape = ROCK;
-            }
-            break;
+               //need to win
+               if (scoreOpponent == ROCK)    scoreShape = PAPER;
+               if (scoreOpponent == PAPER)   scoreShape = SCISSOR;
+               if (scoreOpponent == SCISSOR) scoreShape = ROCK;
+               break;
          }
          int score = scoreShape;
          if (scoreShape == scoreOpponent){
             score += 3;
          }
-         if (scoreShape == ROCK && scoreOpponent == SCISSOR){
+         if (   (scoreShape == ROCK    && scoreOpponent == SCISSOR)
+             || (scoreShape == PAPER   && scoreOpponent == ROCK)
+             || (scoreShape == SCISSOR && scoreOpponent == PAPER)){
             score += 6;
          }
-         if (scoreShape == PAPER && scoreOpponent == ROCK){
-            score += 6;
-         }
-         if (scoreShape == SCISSOR && scoreOpponent == PAPER){
-            score += 6;
-         }
-         if (scoreShape == ROCK && scoreOpponent == PAPER){
-            score += 0;
-         }
-         if (scoreShape == PAPER && scoreOpponent == SCISSOR){
-            score += 0;
-         }
-         if (scoreShape == SCISSOR && scoreOpponent == ROCK){
+         if (   (scoreShape == ROCK    && scoreOpponent == PAPER)
+             || (scoreShape == PAPER   && scoreOpponent == SCISSOR)
+             || (scoreShape == SCISSOR && scoreOpponent == ROCK)){
             score += 0;
          }
          totalScore += (long)score;
---
+++ src/main/java/be/dno/advent2022/Day03.java
@@ -0,0 +1,27 @@
+package be.dno.advent2022;
+
+import be.dno.Day;
+
+public class Day03 extends Day {
+
+
+   public Day03(){
+      fileName = "2022/day03.txt";
+   }
+
+   @Override
+   public void fillDataStruct() {
+
+   }
+
+
+   @Override
+   public String processPart1() {
+      return "";
+   }
+
+   @Override
+   public String processPart2() {
+      return "";
+   }
+}
