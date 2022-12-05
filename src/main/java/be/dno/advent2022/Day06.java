29c46b003fdd4a272a68c5b02d266787067c3501
Didier Nowak <didier.nowak@ctie.etat.lu>
2022-12-05 07:33:49 +0100

    day06 preparation

---
+++ src/main/java/be/dno/advent2022/Day06.java
@@ -0,0 +1,31 @@
+package be.dno.advent2022;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+import be.dno.Day;
+import be.dno.Utils;
+
+public class Day06 extends Day {
+   ArrayDeque<String>[] crates;
+   public Day06(){
+      fileName = "2022/day06.txt";
+   }
+
+   @Override
+   public void fillDataStruct() {
+
+   }
+
+
+   @Override
+   public String processPart1() {
+      return "";
+   }
+
+   @Override
+   public String processPart2() {
+      return "";
+   }
+}
+
